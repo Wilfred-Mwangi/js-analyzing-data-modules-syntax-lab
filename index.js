907dc88bb58d859fd51562e54115df7c337357bd
Wilfred Mwangi <wilfredmwangi050@gmail.com>
2026-04-19 22:46:06 +0300

    Complete combineUsers function

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('datejs');
 
+function allUsers(...args) {
+  let combinedObject = {
+    users: []
+  };
+  
+  for (let i = 0; i < args.length; i++) {
+    let currentBatch = args[i];
+    combinedObject.users = [...combinedObject.users, ...currentBatch];
+  }
+  
+  let now = new Date();
+  combinedObject.merge_date = now.toString('M/d/yyyy');
+  
+  return combinedObject;
+}
 
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = allUsers;
