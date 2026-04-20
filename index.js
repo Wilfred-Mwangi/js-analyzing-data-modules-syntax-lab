237e0ffead3f94e3309ccd01745eda5eb3e72ae7
Wilfred Mwangi <wilfredmwangi050@gmail.com>
2026-04-20 15:06:59 +0300

    add combineUsers function with spread and datejs

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 require('datejs');
 
-function allUsers(...args) {
-  let combinedObject = {
-    users: []
-  };
-  
-  for (let i = 0; i < args.length; i++) {
-    let currentBatch = args[i];
-    combinedObject.users = [...combinedObject.users, ...currentBatch];
-  }
-  
-  let now = new Date();
-  combinedObject.merge_date = now.toString('M/d/yyyy');
-  
-  return combinedObject;
+function combineUsers(...args) {
+    let combinedObject = {
+        users: []
+    };
+    
+    for (let i = 0; i < args.length; i++) {
+        combinedObject.users = [...combinedObject.users, ...args[i]];
+    }
+    
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    
+    return combinedObject;
 }
 
-module.exports = allUsers;
+module.exports = { combineUsers };
